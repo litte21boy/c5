7c1a3b6b498079980c0bb9a6182e19e2412c5d55
Your Name <you@example.com>
2019-01-18 10:16:14 +0800

    完成应用的功能框架

--- main.c
+++ main.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("xueshengchengjiguanlixitong\n");
+    while (1)
+    {
+
+        printf("1---tianjiaxueshengchengji\n");
+        printf("2---shanchuxueshengchengji\n");
+        printf("3---chakansuoyouxueshengchengji\n");
+        printf("4---yewu1:suoyouzongfengbujigedechengji\n");
+        printf("5---yewu2:meiyikedoubujigedexueshengchengji\n");
+        printf("6---yewu3:xianshizongfengzuigaochenhji\n");
+        printf("7---yewu4:xspjcj\n");
+        printf("8---tuichuchengxu\n");
+
+        printf("qinghsurugongnengbianhao\n");
+        int code;
+        scanf("%d", &code);
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+        if (code == 6)
+        {
+        }
+        if (code == 7)
+        {
+        }
+        if (code == 8)
+        {
+            printf("tuichuchengji\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
