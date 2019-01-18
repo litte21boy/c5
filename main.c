d511284f48035500ed92e7a72a5d2ad525821388
Your Name <you@example.com>
2019-01-18 15:32:34 +0800

    完成学生成绩管理系统

--- main.c
+++ main.c
@@ -72,10 +72,10 @@ int main()
 
             for (int i = 0; i < index; i++)
             {
-                printf("di%dgexuesheng%sdegekechengjiruxia:\n", i + 1, arr[i].name );
-                printf("yuwengcehngjiwei%d\n",arr[i].yuwen);
-                printf("shuxuechengjiwei%d\n",arr[i].shuxue);
-                printf("yingyucehngjiwei%d\n",arr[i].yingyu);
+                printf("di%dgexuesheng%sdegekechengjiruxia:\n", i + 1, arr[i].name);
+                printf("yuwengcehngjiwei%d\n", arr[i].yuwen);
+                printf("shuxuechengjiwei%d\n", arr[i].shuxue);
+                printf("yingyucehngjiwei%d\n", arr[i].yingyu);
             }
             printf("dianjihuichejixu\n");
             char x;
@@ -84,22 +84,89 @@ int main()
         }
         if (code == 4)
         {
+            printf("suoyouzongfengbijigedexueshengchengjiruxia:\n");
+
+            for (int i = 0; i < index; i++)
+            {
+                int sum = arr[i].yuwen + arr[i].shuxue + arr[i].yingyu;
+                if (sum < 180)
+                {
+                    printf("zongfendiyu180detongxueshi%s\nyuwen%dshuxue%dyingyu%d", arr[i].name, arr[i].yuwen, arr[i].shuxue, arr[i].yingyu);
+                }
+            }
+            printf("dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 5)
         {
+            printf("meiyikedoubujigedexueshengruxia:\n");
+
+            for (int i = 0; i < index; i++)
+            {
+                if (arr[i].yuwen < 60 && arr[i].shuxue < 60 && arr[i].yingyu < 60)
+                {
+                    printf("sandoubujigedetongxueshi%s\nyuwen%dshuxue%dyingyu%d", arr[i].name, arr[i].yuwen, arr[i].shuxue, arr[i].yingyu);
+                }
+            }
+            printf("dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 6)
         {
+            printf("zongengzuigaodechengwei:\n");
+            int max = 0;
+            int stuID = 0;
+
+            for (int i = 0; i < index; i++)
+            {
+                int sum = arr[i].yuwen + arr[i].shuxue + arr[i].yingyu;
+                if (max < sum)
+                {
+                    max = sum;
+                    stuID = i;
+                }
+            }
+            printf("zongfengzuigaodedetongxueshi%s\nyuwen%dshuxue%dyingyu%d", arr[stuID ].name, arr[stuID ].yuwen, arr[stuID ].shuxue, arr[stuID ].yingyu);
+
+            printf("dianjihuichejixu\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 7)
         {
-        }
-        if (code == 8)
-        {
-            printf("tuichuchengji\n");
-            break;
-        }
-    }
+            double max = 0.0;
+            int stuID = 0;
 
-    return 0;
+            for (int i = 0; i < index; i++)
+            {
+                if (arr[i].yuwen >= 60 && arr[i].shuxue >= 60 && arr[i].yingyu >= 60)
+                {
+                    double avg = (arr[i].yuwen + arr[i].shuxue + arr[i].yingyu) / 3.0;
+                    if (max < avg)
+                    {
+                        max = avg;
+                        stuID = i;
+                    }
+                }
+                printf("平均分最高且没有不及格科目的学生姓名为%s，语文：%d，数学%d，英语%d\n", arr[stuID].name, arr[stuID].yuwen, arr[stuID].shuxue, arr[stuID].yingyu);
+
+                printf("\n点击回车继续\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            if (code == 8)
+            {
+                printf("tuichuchengji\n");
+                break;
+            }
+        }
+
+        return 0;
+    }
 }
